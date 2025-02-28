06fd6f046b2c94a356f35c71f422acde821fd3de
worstcase-scenario <iamspamalot@gmx.de>
2025-02-28 11:01:31 +0100

    Fix: Update package.mk to use worstcase-scenario-FR-patch

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,8 +2,9 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="e46506db99d688be25f08624f319fd2a77c89fb2"
-PKG_GIT_CLONE_BRANCH="EmuELEC"
+#PKG_VERSION="e46506db99d688be25f08624f319fd2a77c89fb2"
+PKG_VERSION="d6341f429a444c0fcb42cfca642feb1f2177f05a"
+PKG_GIT_CLONE_BRANCH="worstcase-scenario-FR-patch"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
