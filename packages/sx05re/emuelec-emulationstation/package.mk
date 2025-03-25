a3379180bc2e0f212969a1c65705ae622957ef84
worstcase-scenario <iamspamalot@gmx.de>
2025-03-25 09:13:42 +0100

    Newest PKG_VERSION

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="cec122e907c6e2333ba8315c4837e79ef6a51d98"
+PKG_VERSION="2ad22224595207fac2d6b07b5c7d26c150323465"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
