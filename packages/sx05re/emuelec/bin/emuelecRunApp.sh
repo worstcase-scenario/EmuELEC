b6adefa1264af23e6ef08da5e9d29ff6aada1554
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2025-09-22 16:07:26 +0200

    Propagate dynamic splash handling to emuelecRunApp

--- packages/sx05re/emuelec/bin/emuelecRunApp.sh
+++ packages/sx05re/emuelec/bin/emuelecRunApp.sh
@@ -9,6 +9,28 @@
 
 arguments="$@"
 
+# Configure dynamic splash handling before starting the splash
+SPLASH_DYNAMIC_RAW=$(get_ee_setting ee_splash.dynamic_stop)
+case "${SPLASH_DYNAMIC_RAW,,}" in
+    1|true|yes|on|enabled)
+        SPLASH_DYNAMIC="1"
+        ;;
+    *)
+        SPLASH_DYNAMIC="0"
+        ;;
+esac
+
+if [[ "${SPLASH_DYNAMIC}" == "1" ]]; then
+    export EE_SPLASH_DYNAMIC="1"
+    EE_SPLASH_PATTERN=$(get_ee_setting ee_splash.dynamic_stop_pattern)
+    export EE_SPLASH_PATTERN
+    EE_SPLASH_TIMEOUT=$(get_ee_setting ee_splash.dynamic_timeout)
+    export EE_SPLASH_TIMEOUT
+    export EE_SPLASH_STANDALONE="1"
+else
+    unset EE_SPLASH_DYNAMIC EE_SPLASH_PATTERN EE_SPLASH_TIMEOUT EE_SPLASH_STANDALONE
+fi
+
 # Extract the platform name from the arguments
 PLATFORM="${arguments##*-P}"  # read from -P onwards
 PLATFORM="${PLATFORM%% *}"  # until a space is found
@@ -18,7 +40,11 @@ ROMNAME="${1}"
 init_game
 emuelec-utils init_app_video "${PLATFORM}" "${ROMNAME}"
 
-"$@"
+if [[ "${SPLASH_DYNAMIC}" == "1" ]]; then
+    ee_splash_wrapper.sh "$@"
+else
+    "$@"
+fi
 
 emuelec-utils end_app_video
 end_game
