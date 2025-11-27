e20ec49932ac8547f272a2950154e3f1ce4217da
worstcase-scenario <iamspamalot@gmx.de>
2025-11-27 16:33:53 +0100

    minivmac - libretro core, an emulator for Macintosh

---
+++ packages/sx05re/libretro/minivmac/package.mk
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# EmuELEC package for Mini vMac (libretro-minivmac) core
+
+PKG_NAME="minivmac"
+PKG_VERSION="e7fcfef"
+
+PKG_LICENSE="GPL-2.0-only"
+PKG_SITE="https://github.com/libretro/libretro-minivmac"
+PKG_URL="${PKG_SITE}.git"
+
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emuelec/libretro"
+PKG_SHORTDESC="Mini vMac Macintosh II emulator (libretro core)"
+PKG_LONGDESC="libretro-minivmac is a libretro port of Mini vMac, a classic Macintosh II emulator."
+PKG_TOOLCHAIN="make"
+
+PKG_IS_CORE="yes"
+
+pre_make_target() {
+  cd "${PKG_BUILD}"
+
+  # Remove hardcoded optimization flags from the Makefile
+  # so EmuELEC's own CFLAGS are used.
+  if [ -f Makefile ]; then
+    sed -i 's/-O[0123s]//g; s/-Ofast//g' Makefile || true
+  fi
+}
+
+make_target() {
+  make -C "${PKG_BUILD}"
+}
+
+makeinstall_target() {
+  mkdir -p "${INSTALL}/usr/lib/libretro"
+  cp "${PKG_BUILD}/minivmac_libretro.so" \
+     "${INSTALL}/usr/lib/libretro/"
+}
