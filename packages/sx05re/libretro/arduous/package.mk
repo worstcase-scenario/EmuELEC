340f89cc0e1b39b86d0dbecef6256351841c2a9c
worstcase-scenario <iamspamalot@gmx.de>
2025-12-22 12:12:08 +0100

    cleaned and revised version

--- packages/sx05re/libretro/arduous/package.mk
+++ packages/sx05re/libretro/arduous/package.mk
@@ -1,76 +1,27 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present
 
 PKG_NAME="arduous"
-PKG_VERSION="main"
-PKG_SHA256="5c9ecda60735aab6d3e26ed78f1fc266cb0f45635ecf7ff1a3bd29aeddf9db63"
-
+PKG_VERSION="50c1e48084f003dee582ed5f4f5c0f59eb30bc4c"
 PKG_ARCH="any"
-PKG_LICENSE="GPLv3"
+PKG_LICENSE="GPL-3.0-or-later"
+
 PKG_SITE="https://github.com/libretro/arduous"
-PKG_URL="https://github.com/libretro/arduous/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="${PKG_SITE}.git"
 
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="emuelec/libretro"
 PKG_SHORTDESC="Arduous libretro core (Arduboy)"
-PKG_LONGDESC="Arduous is a libretro core for the Arduboy handheld game console."
-
-PKG_TOOLCHAIN="manual"
-
-configure_target() {
-  cd "${PKG_BUILD}"
-
-
-  if [ ! -f "simavr/simavr/sim/avr_acomp.c" ]; then
-    rm -rf simavr
-    mkdir -p simavr
-
-    echo "Arduous: fetching simavr source ..."
-    if command -v curl >/dev/null 2>&1; then
-      curl -L -o simavr.tar.gz \
-        "https://github.com/buserror/simavr/archive/refs/heads/master.tar.gz"
-    else
-      wget -O simavr.tar.gz \
-        "https://github.com/buserror/simavr/archive/refs/heads/master.tar.gz"
-    fi
-
-    tar -xf simavr.tar.gz --strip-components=1 -C simavr
-    rm -f simavr.tar.gz
-  fi
-
-
-  if ! grep -q "ihex_chunk_s" src/arduous/arduous.cpp; then
-    sed -i '1i\
-typedef struct ihex_chunk_s {\
-    unsigned long baseaddr;\
-    unsigned int size;\
-    unsigned char *data;\
-} ihex_chunk_t;\
-\
-typedef ihex_chunk_t* ihex_chunk_p;\
-\
-static inline void free_ihex_chunks(ihex_chunk_p chunks) {\
-    (void)chunks;\
-}\
-' src/arduous/arduous.cpp
-  fi
-
-  mkdir -p build
-  cd build
-
-  cmake .. \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DLIBRETRO=ON \
-    -DCMAKE_C_COMPILER="${CC}" \
-    -DCMAKE_CXX_COMPILER="${CXX}"
-}
-
-make_target() {
-  cd "${PKG_BUILD}/build"
-  make
-}
+PKG_LONGDESC="Arduous is a libretro emulator core for the Arduboy."
+PKG_TOOLCHAIN="cmake"
+PKG_IS_CORE="yes"
 
 makeinstall_target() {
   mkdir -p "${INSTALL}/usr/lib/libretro"
-  cp "${PKG_BUILD}/build/arduous_libretro.so" \
-     "${INSTALL}/usr/lib/libretro/"
+
+  if [ -f "${PKG_BUILD}/.${TARGET_NAME}/arduous_libretro.so" ]; then
+    cp "${PKG_BUILD}/.${TARGET_NAME}/arduous_libretro.so" "${INSTALL}/usr/lib/libretro/"
+  else
+    cp "${PKG_BUILD}/arduous_libretro.so" "${INSTALL}/usr/lib/libretro/"
+  fi
 }
