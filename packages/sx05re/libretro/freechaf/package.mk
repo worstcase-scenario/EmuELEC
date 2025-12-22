fb3b463e57c347a9671a621fc9dafd2a99b47c5a
worstcase-scenario <iamspamalot@gmx.de>
2025-12-22 11:20:05 +0100

    cleaned up package.mk

--- packages/sx05re/libretro/freechaf/package.mk
+++ packages/sx05re/libretro/freechaf/package.mk
@@ -3,11 +3,11 @@
 
 PKG_NAME="freechaf"
 PKG_VERSION="cdb8ad6fcecb276761b193650f5ce9ae8b878067"
-PKG_SHA256="4e8be1cb01f974e2bc34e87d80a8c75cf6345a7542a11d61fb1150e0421dda1d"
+PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/FreeChaF"
-PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_URL="${PKG_SITE}.git"
 
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="emuelec/libretro"
@@ -15,44 +15,15 @@ PKG_SHORTDESC="FreeChaF libretro core (Fairchild Channel F)"
 PKG_LONGDESC="FreeChaF is a libretro emulation core for the Fairchild Channel F / Video Entertainment System."
 
 PKG_TOOLCHAIN="make"
+PKG_GIT_CLONE_SINGLE="yes"
+PKG_MAKE_OPTS_TARGET="platform=unix"
 
-pre_configure_target() {
-  cd "$PKG_BUILD"
-
-
-  if [ ! -d "src/deps/libretro-common" ] || [ ! -f "src/deps/libretro-common/include/libretro.h" ]; then
-    echo "freechaf: fetching libretro-common sources..."
-    rm -rf src/deps/libretro-common
-    mkdir -p src/deps/libretro-common
-
-
-    LIBRETRO_COMMON_TAR="https://github.com/libretro/libretro-common/archive/refs/heads/master.tar.gz"
-
-
-    if ! wget -O libretro-common.tar.gz "$LIBRETRO_COMMON_TAR"; then
-      curl -L -o libretro-common.tar.gz "$LIBRETRO_COMMON_TAR"
-    fi
-
-    tar -xzf libretro-common.tar.gz --strip-components=1 -C src/deps/libretro-common
-    rm -f libretro-common.tar.gz
-  fi
+post_unpack_target() {
+  cd "${PKG_BUILD}"
+  git submodule update --init --recursive
 }
-
-make_target() {
-  cd "$PKG_BUILD"
-
-
-  EXTRA_CFLAGS="-Isrc/deps/libretro-common/include -Isrc/deps/libretro-common"
-
-  make \
-    CC="$CC" \
-    CFLAGS="$CFLAGS $EXTRA_CFLAGS" \
-    LDFLAGS="$LDFLAGS" \
-    platform="${LIBRETRO_PLATFORM:-unix}"
-}
-
 
 makeinstall_target() {
-  mkdir -p "$INSTALL/usr/lib/libretro"
-  cp "$PKG_BUILD/freechaf_libretro.so" "$INSTALL/usr/lib/libretro/"
+  mkdir -p "${INSTALL}/usr/lib/libretro"
+  cp "${PKG_BUILD}/freechaf_libretro.so" "${INSTALL}/usr/lib/libretro/"
 }
