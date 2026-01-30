b4671276076c88a29c4691476e7acf75c61d0b3d
worstcase-scenario <iamspamalot@gmx.de>
2026-01-30 12:10:06 +0100

    updated version, solved path problem when starting games

--- packages/sx05re/emulators/x16/scripts/x16emustart.sh
+++ packages/sx05re/emulators/x16/scripts/x16emustart.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
-
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2025-present Team EmuELEC
-
-# Commander X16 Emulator Start Script
+# Wrapper-script for X16emu
+# Copyright (C) 2025-present worstcase_scenario (https://github.com/worstcase-scenario)
 
 SYSTEM_ROM="/usr/share/x16-emulator/rom.bin"
 USER_ROM="/storage/roms/x16/rom.bin"
 EMU_BIN="/usr/bin/x16emu"
 
 if [ -f "${USER_ROM}" ]; then
-    ROM="${USER_ROM}"
+  ROM="${USER_ROM}"
 elif [ -f "${SYSTEM_ROM}" ]; then
-    ROM="${SYSTEM_ROM}"
+  ROM="${SYSTEM_ROM}"
 else
-    echo "ERROR: No rom.bin found!"
-    exit 1
+  echo "ERROR: No rom.bin found!"
+  exit 1
 fi
 
-PARAMS="-rom ${ROM}"
-PARAMS="${PARAMS} -fullscreen"
-PARAMS="${PARAMS} -quality best"
-PARAMS="${PARAMS} -scale 2"
-
-PARAMS="${PARAMS} -joy1 -joy2 -joy3 -joy4"
+ARGS=(-rom "${ROM}" -fullscreen -quality best -scale 2 -joy1 -joy2 -joy3 -joy4)
 
 if [ -n "$1" ]; then
-    FILE="$1"
-    EXT="${FILE##*.}"
-    
-    case "${EXT,,}" in
-        prg)
-            PARAMS="${PARAMS} -prg ${FILE} -run"
-            ;;
-        bas)
-            PARAMS="${PARAMS} -bas ${FILE} -run"
-            ;;
-        img)
-            PARAMS="${PARAMS} -sdcard ${FILE}"
-            ;;
-        crt)
-            PARAMS="${PARAMS} -cart ${FILE}"
-            ;;
-    esac
+  FILE="$1"
+  EXT="${FILE##*.}"
+
+  case "${EXT,,}" in
+    prg|bas)
+      GAME_DIR="$(dirname -- "${FILE}")"
+      GAME_BASE="$(basename -- "${FILE}")"
+      cd "${GAME_DIR}" || exit 1
+      ;;
+  esac
+
+  case "${EXT,,}" in
+    prg)
+      ARGS+=(-prg "${GAME_BASE}" -run)
+      ;;
+    bas)
+      ARGS+=(-bas "${GAME_BASE}" -run)
+      ;;
+    img)
+      ARGS+=(-sdcard "${FILE}")
+      ;;
+    crt)
+      ARGS+=(-cart "${FILE}")
+      ;;
+  esac
 fi
 
-exec ${EMU_BIN} ${PARAMS}
+exec "${EMU_BIN}" "${ARGS[@]}"
