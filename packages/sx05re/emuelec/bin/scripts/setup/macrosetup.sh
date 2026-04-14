7e081a597d5d7b0012d82c099961382aeebd6c85
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2026-04-14 19:46:54 +0200

    Add creation comment to macrosetup.sh
    Added a comment indicating the file was created by Claude.AI.

--- packages/sx05re/emuelec/bin/scripts/setup/macrosetup.sh
+++ packages/sx05re/emuelec/bin/scripts/setup/macrosetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2026-present worstcase_scenario (https://github.com/worstcase-scenario)
-
+# THIS FILE HAS BEEN CREATED BY CLAUDE.AI
 . /etc/profile
 
 SCRIPT="/usr/bin/scripts/setup/macrosetup.py"
