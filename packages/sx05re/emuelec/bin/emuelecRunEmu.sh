6ce723b3eb2691580eccb15218fbed6a30115434
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2026-04-14 19:37:50 +0200

    Added eka2l1 standalone emulator for N-GAGE  / added eka2l1 commander (#1623)
    * Added eka2l1 standalone emulator for ngage / added eka2l1 commander, a swiss-army-knife tool for different tasks for eka2l1 / added and updated system/setup files for emuelec to integrate the emulator and the script into EmuELEC

--- packages/sx05re/emuelec/bin/emuelecRunEmu.sh
+++ packages/sx05re/emuelec/bin/emuelecRunEmu.sh
@@ -314,6 +314,12 @@ case ${PLATFORM} in
             RUNTHIS='${TBASH} ppsspp.sh "${ROMNAME}"'
                 fi
                 ;;
+		"ngage")
+		if [ "$EMU" = "eka2l1" ]; then
+            set_kill_keys "eka2l1"
+            RUNTHIS='${TBASH} ekastart.sh "${ROMNAME}"'
+        fi
+               ;;
         "neocd")
                 if [ "${EMU}" = "fbneo" ]; then
             RUNTHIS='${RABIN} ${VERBOSE} -L /tmp/cores/fbneo_libretro.so --subsystem neocd --config ${RACONF} "${ROMNAME}"'
