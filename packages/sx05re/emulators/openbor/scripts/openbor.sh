5b3417dfad9e6f089eb773c4668cd029d0e6a1fe
Joshua L <Langerz82@users.noreply.github.com>
2026-09-23 16:02:35 +1000

    Update openbor.sh

--- packages/sx05re/emulators/openbor/scripts/openbor.sh
+++ packages/sx05re/emulators/openbor/scripts/openbor.sh
@@ -22,17 +22,37 @@ SAVES="${CONFIGDIR}/Saves"
 # make a symlink to the pak
     ln -sf "${1}" "${PAKS}"
 
-# create symlink to master.cfg
-rm "${SAVES}/${pakname}.cfg"
+# remove game symlink if exists
+if [ -L "${SAVES}/${pakname}.cfg" ]; then
+    rm "${SAVES}/${pakname}.cfg"
+fi
+
+# copy master.cfg to master game config if no file present
+if [ ! -f "${SAVES}/${pakname}.cfg" ]; then
+    if [ ${OB} = "OpenBORff" ]; then
+         cp -f "${CONFIGDIR}/masterff.cfg" "${SAVES}/${pakname}_masterff.cfg"
+    else
+         cp -f "${CONFIGDIR}/master.cfg" "${SAVES}/${pakname}_master.cfg"
+    fi
+fi
+
+# copy game config from game _master.cfg
 if [ ${OB} = "OpenBORff" ]; then
-     ln -sf "${CONFIGDIR}/masterff.cfg" "${SAVES}/${pakname}.cfg"
+     cp -f "${SAVES}/${pakname}_masterff.cfg" "${SAVES}/${pakname}.cfg"
 else
-     ln -sf "${CONFIGDIR}/master.cfg" "${SAVES}/${pakname}.cfg"
+     cp -f "${SAVES}/${pakname}_master.cfg" "${SAVES}/${pakname}.cfg"
 fi
 
 # Run OpenBOR in the config folder
     cd "${CONFIGDIR}"
 	SDL_AUDIODRIVER=alsa ${OB}
+
+# copy game config to _master.cfg
+if [ ${OB} = "OpenBORff" ]; then
+     cp -f "${SAVES}/${pakname}.cfg" "${SAVES}/${pakname}_masterff.cfg"
+else
+     cp -f "${SAVES}/${pakname}.cfg" "${SAVES}/${pakname}_master.cfg"
+fi
 
 # Clear PAKS folder to avoid getting the launcher on nex run
 rm -rf ${PAKS}/*
