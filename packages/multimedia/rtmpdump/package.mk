7f17557afeb0207af52981f9af890b0e3ff7a538
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2026-09-26 11:23:00 +0200

    Fix three broken package sources: rtmpdump, diffutils, mupen64plus-nx-alt

--- packages/multimedia/rtmpdump/package.mk
+++ packages/multimedia/rtmpdump/package.mk
@@ -4,10 +4,11 @@
 
 PKG_NAME="rtmpdump"
 PKG_VERSION="c5f04a58fc2aeea6296ca7c44ee4734c18401aa3"
-PKG_SHA256="fd8c21263d93fbde8bee8aa6c5f6a657789674bb0f9e74f050651504d5f43b46"
 PKG_LICENSE="GPL"
 PKG_SITE="http://rtmpdump.mplayerhq.hu/"
-PKG_URL="http://repo.or.cz/rtmpdump.git/snapshot/${PKG_VERSION}.tar.gz"
+PKG_URL="https://repo.or.cz/rtmpdump.git"
+PKG_GIT_CLONE_BRANCH="master"
+GET_HANDLER_SUPPORT="git"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
 PKG_LONGDESC="rtmpdump is a toolkit for RTMP streams."
 PKG_BUILD_FLAGS="+pic"
